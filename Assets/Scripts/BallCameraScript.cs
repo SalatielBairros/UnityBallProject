834eca4bb040adc49e4afc93493f13cab5192163
Salatiel Bairros <salatiel.costabairros@gmail.com>
2018-12-01 17:58:02 -0200

    Inclusão dos prefabs e componentes externos

---
+++ Assets/Scripts/BallCameraScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// This script associates the camera with the player (ball), keeping the same distance between both for all game.   
+/// </summary>
+public class BallCameraScript : MonoBehaviour
+{
+    /// <summary>
+    /// The player of the game (in this case, the ball)
+    /// </summary>
+    public GameObject Player;
+
+    /// <summary>
+    /// Distance between the player and the camera.
+    /// </summary>
+    private Vector3 offset;
+
+    void Start()
+    {
+        //Get the offset (the initial distance) between camera and player. 
+        offset = transform.position - Player.transform.position;
+    }
+
+    /// <summary>
+    /// LateUpdate happens each frame after all update methods of all gameobjects were called.
+    /// </summary>
+    void LateUpdate()
+    {
+        //Keep the distance between the camera and the player the same.
+        transform.position = Player.transform.position + offset;
+    }
+}
