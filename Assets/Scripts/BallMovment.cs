4c7cb7426a946a606fd2ec3dfff0773b63833b99
Salatiel Bairros <salatiel.costabairros@gmail.com>
2018-12-02 01:18:04 -0200

    Criação de regras de score no player

--- Assets/Scripts/BallMovment.cs
+++ Assets/Scripts/BallMovment.cs
@@ -1,27 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
-  public class BallMovment : MonoBehaviour
-  {
-    public float speed;
-    private Rigidbody ball;
-
-    void Start()
+    public class BallMovment : MonoBehaviour
     {
-      ball = GetComponent<Rigidbody>();
-    }
+        public float Speed;
+        public TextMesh CountText;
 
-    void FixedUpdate()
-    {
-      ball.AddForce(GetMovementDirection() * speed);
-    }
+        private Rigidbody _ball;
+        private int _bonusCount;
 
-    private Vector3 GetMovementDirection()
-    {
-      float moveHorinzontal = Input.GetAxis("Horizontal");
-      float moveVertical = Input.GetAxis("Vertical");
-      return new Vector3(moveHorinzontal, 0.0f, moveVertical);
+        void Start()
+        {
+            _ball = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            _ball.AddForce(GetMovementDirection() * Speed);
+        }
+
+        private Vector3 GetMovementDirection()
+        {
+            float moveHorinzontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            return new Vector3(moveHorinzontal, 0.0f, moveVertical);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("BonusCube"))
+            {
+                var bonusCube = other.gameObject.GetComponent("BonusCube") as BonusCube;
+                _bonusCount += (bonusCube != null ? bonusCube.BonusValue : 1);
+
+                CountText.text = _bonusCount.ToString();
+                Destroy(other.gameObject);
+            }
+        }
     }
-  }
 }
---
+++ Assets/Scripts/BonusCube.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusCube : MonoBehaviour
+{
+    public int BonusValue = 1;
+}
